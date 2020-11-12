2d57695ed6e7cff0724d8bca59d5ac7564fef212
Daniel Pires <danielpires6@hotmail.com>
2020-11-12 20:32:31 -0300

    feat: Adicionado projeto

---
+++ node/mock_pokemon_flutter.js
@@ -0,0 +1,9 @@
+module.exports = [
+    {
+        "name": "bulbasaur",
+        "data": "https://pokeapi.co/api/v2/pokemon/1/",
+        "photo": "https://raw.githubusercontent.com/ZeChrales/PogoAssets/master/pokemon_icons/pokemon_icon_004_00_shiny.png",
+        "type": "grass",
+    },
+   
+]

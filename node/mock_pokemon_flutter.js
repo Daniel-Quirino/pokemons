1cfd6f83953b855948df93c326b65529f21b6021
Daniel Pires <danielpires6@hotmail.com>
2020-11-12 21:15:20 -0300

    feat: Adicionado mais pokemons

--- node/mock_pokemon_flutter.js
+++ node/mock_pokemon_flutter.js
@@ -2,8 +2,74 @@ module.exports = [
     {
         "name": "bulbasaur",
         "data": "https://pokeapi.co/api/v2/pokemon/1/",
-        "photo": "https://raw.githubusercontent.com/ZeChrales/PogoAssets/master/pokemon_icons/pokemon_icon_004_00_shiny.png",
+        "photo": "https://raw.githubusercontent.com/Daniel-Quirino/pokemons/master/node/assets/bulbasaur.png",
         "type": "grass",
     },
-   
+    {
+        "name": "butterfree",
+        "data": "https://pokeapi.co/api/v2/pokemon/12/",
+        "photo": "https://raw.githubusercontent.com/Daniel-Quirino/pokemons/master/node/assets/butterfree.png",
+        "type": "bug",
+        "trainer": "Roberth"
+    },
+    {
+        "name": "charmander",
+        "data": "https://pokeapi.co/api/v2/pokemon/4/",
+        "photo": "https://raw.githubusercontent.com/Daniel-Quirino/pokemons/master/node/assets/charmander.png",
+        "type": "fire"
+    },
+    {
+        "name": "metapod",
+        "data": "https://pokeapi.co/api/v2/pokemon/11/",
+        "photo": "https://raw.githubusercontent.com/Daniel-Quirino/pokemons/master/node/assets/metapod.png",
+        "type": "bug",
+    },
+    {
+        "name": "pidgey",
+        "data": "https://pokeapi.co/api/v2/pokemon/16/",
+        "photo": "https://raw.githubusercontent.com/Daniel-Quirino/pokemons/master/node/assets/pidgey.png",
+        "type": "normal"
+    },
+    {
+        "name": "pikachu",
+        "data": "https://pokeapi.co/api/v2/pokemon/25/",
+        "photo": "https://raw.githubusercontent.com/Daniel-Quirino/pokemons/master/node/assets/pikachu.png",
+        "type": "electric"
+    },
+    {
+        "name": "squirtle",
+        "data": "https://pokeapi.co/api/v2/pokemon/7/",
+        "photo": "https://raw.githubusercontent.com/Daniel-Quirino/pokemons/master/node/assets/squirtle.png",
+        "type": "water"
+    },
+    {
+        "name": "haunter",
+        "data": "https://pokeapi.co/api/v2/pokemon/93/",
+        "photo": "https://raw.githubusercontent.com/Daniel-Quirino/pokemons/master/node/assets/haunter.png",
+        "type": "ghost"
+    },
+    {
+        "name": "krabby",
+        "data": "https://pokeapi.co/api/v2/pokemon/98/",
+        "photo": "https://raw.githubusercontent.com/Daniel-Quirino/pokemons/master/node/assets/krabby.png",
+        "type": "water"
+    },
+    {
+        "name": "muk",
+        "data": "https://pokeapi.co/api/v2/pokemon/89/",
+        "photo": "https://raw.githubusercontent.com/Daniel-Quirino/pokemons/master/node/assets/muk.png",
+        "type": "poison"
+    },
+        {
+        "name": "raticate",
+        "data": "https://pokeapi.co/api/v2/pokemon/20/",
+        "photo": "https://raw.githubusercontent.com/Daniel-Quirino/pokemons/master/node/assets/raticate.png",
+        "type": "normal"
+    },
+    {
+        "name": "primeape",
+        "data": "https://pokeapi.co/api/v2/pokemon/57/",
+        "photo": "https://raw.githubusercontent.com/Daniel-Quirino/pokemons/master/node/assets/primeape.png",
+        "type": "fighting"
+    }
 ]
